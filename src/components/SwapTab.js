55294dc78e80b289a5992b91380483323c8e4e1b
Robert Zaremba <robert@zaremba.ch>
2020-11-18 17:23:19 +0100

    Move webapp from github.com/robert-zaremba/near-clp

---
+++ src/components/SwapTab.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+
+import SwapInputCards from "./SwapInputCards";
+
+import Button from 'react-bootstrap/Button';
+
+import { BsArrowUpDown } from "react-icons/bs";
+
+export default function SwapTab() {
+
+  return (
+    <>
+      <SwapInputCards/>
+    </>
+  );
+}
