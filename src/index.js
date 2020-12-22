ac35a208649905f7b09ae55b5f4b55051c39acd8
Robert Zaremba <robert@zaremba.ch>
2020-12-22 21:01:23 +0100

    cleanup loadTokenList

--- src/index.js
+++ src/index.js
@@ -9,56 +9,43 @@ import { Web3Provider } from './contexts/Web3Context';
 import { TokenListProvider } from './contexts/TokenListContext';
 import { NotificationProvider } from './contexts/NotificationContext';
 
-initToken().then(value => {
-  console.log("Finsihed Loading Token List!!");
 
-  window.nearInitPromise = initContract()
-  .then(() => {
-    console.log("RENDERING:");
-    ReactDOM.render(
-      <InputsProvider>
-        <Web3Provider>
-          <TokenListProvider>
-            <NotificationProvider>
-              <ThemeProvider>
-                <App />
-              </ThemeProvider>
-            </NotificationProvider>
-          </TokenListProvider>
-        </Web3Provider>
-      </InputsProvider>,
-      document.querySelector('#root')
-    )
-  })
-  .catch(console.error)
-});
-
-async function fetchJson(src) {
-  const response = await fetch(src);
-  return await response.json();
+const tokenListURLs = {
+  'betanet': 'https://ghcdn.rawgit.org/near-clp/token-list/master/beta-net.json',
+  'guildnet': 'https://ghcdn.rawgit.org/near-clp/token-list/master/guild-net.json',
+  'mainnet': 'https://ghcdn.rawgit.org/near-clp/token-list/master/main-net.json',
+  'testnet': 'https://ghcdn.rawgit.org/near-clp/token-list/master/test-net.json'
 }
 
-async function initToken() {
-  switch(window.config.nearNetworkId) {
-  case 'betanet':
-    var tokens = await fetchJson("https://ghcdn.rawgit.org/near-clp/token-list/master/beta-net.json")
-    window.tokens = tokens
-    break;
-  case 'guildnet':
-    var tokens = await fetchJson("https://ghcdn.rawgit.org/near-clp/token-list/master/guild-net.json")
-    window.tokens = tokens
-    break;
-  case 'mainnet':
-    var tokens = await fetchJson("https://ghcdn.rawgit.org/near-clp/token-list/master/main-net.json")
-    window.tokens = tokens
-    break;
-  case 'testnet':
-    var tokens = await fetchJson("https://ghcdn.rawgit.org/near-clp/token-list/master/test-net.json")
-    window.tokens = tokens
-    break;
-  default:
-    break;
+async function loadTokenList() {
+  const url = tokenListURLs[window.config.nearNetworkId];
+  if (url === undefined) {
+    console.error("Unknown network '%s', can't fetch token list", window.config.nearNetworkId);
+    return
   }
+  const response = await fetch(url);
+  window.tokens = await response.json();
   console.log("Loaded Token List: ", window.tokens);
 }
 
+
+loadTokenList().then(() => {
+  window.nearInitPromise = initContract()
+    .then(() => {
+      ReactDOM.render(
+        <InputsProvider>
+          <Web3Provider>
+            <TokenListProvider>
+              <NotificationProvider>
+                <ThemeProvider>
+                  <App />
+                </ThemeProvider>
+              </NotificationProvider>
+            </TokenListProvider>
+          </Web3Provider>
+        </InputsProvider>,
+        document.querySelector('#root')
+      )
+    })
+    .catch(console.error)
+});
